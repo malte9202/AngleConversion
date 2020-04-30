247b485ad95779b1ab39ef3ca4a91a3fbe703d21
malte9202 <61252683+malte9202@users.noreply.github.com>
2020-04-30 22:00:49 +0200

    Update AngleConversion.py

--- AngleConversion.py
+++ AngleConversion.py
@@ -3,38 +3,40 @@ from math import pi
 
 
 class AngleConversion:
-    @staticmethod
-    def get_conversion_type() -> int:
+    @staticmethod  # static method because it is not bound to the class
+    def get_conversion_type() -> int:  # get conversion type from user, type hint: returns int
         conversion_type = None
-        while conversion_type not in [1, 2]:
+        while conversion_type not in [1, 2]:  # while user input is anything else than 1 or 2
             try:
                 conversion_type = int(input("Enter 1 or 2 to choose the type of conversion."
                                             + " Which type of angle conversion do you need?\n(1) rad to degree\n"
                                               "(2) degree to rad\n"))
-            except ValueError:
+            except ValueError:  # except value error for any input which is not int
                 print("Please enter 1 or 2 to choose the type of conversion.")
-            return conversion_type
+        return conversion_type
 
     @staticmethod
-    def get_angle_to_convert() -> float:
+    def get_angle_to_convert() -> float:  # get angle to convert from user,  returns float
         angle_to_convert = None
         while not isinstance(angle_to_convert, float):
             try:
                 angle_to_convert = float(input("Enter the angle you want to convert "))
-            except ValueError:
+            except ValueError:  # except value error for not numerical input, error rises because of float()
                 print("The angle has to be a numeral\nPlease enter a valid value")
         return angle_to_convert
 
     @staticmethod
-    def convert_angle(conversion_type: int, angle_to_convert: float) -> float:
+    def convert_angle(conversion_type: int, angle_to_convert: float) -> float:  # convert angle
         converted_angle = None
-        if conversion_type == 1:
+        if conversion_type == 1:  # rad to degree
             converted_angle = round((360 / (2 * pi)) * angle_to_convert, 2)
-        elif conversion_type == 2:
+        elif conversion_type == 2:  # degree to rad
             converted_angle = round((angle_to_convert / 180) * pi, 2)
         return converted_angle
 
-
+    @staticmethod
+    def print_converted_angle():  # print result + information
+        print(AngleConversion.converted_angle)
 
 
 
